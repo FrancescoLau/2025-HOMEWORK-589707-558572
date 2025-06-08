fb2559290beb34c465346bf7b6ca1623ef1c7a0d
FrancescoLau <laurentifrancesco15@gmail.com>
2025-06-08 20:43:54 +0200

    Add files via upload

---
+++ test/it/uniroma3/diadia/ambienti/CaricatoreLabirintoTest.java
@@ -0,0 +1,70 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class CaricatoreLabirintoTest {
+
+	private final String monolocale = 
+			"Stanze:biblioteca\n"+
+			"Bloccata:\n"+
+			"Buia:\n"+
+			"Magica: \n"+
+			"Inizio:biblioteca\n"+
+			"Vincente:biblioteca\n"+
+			"Cane:\n"+
+			"Strega:\n"+
+			"Mago:\n"+
+			"Attrezzi:\n"+
+			"Uscite:\n";
+
+	private final String bilocale = 
+			"Stanze:N12,N11\n"+
+			"Bloccata:\n"+
+			"Magica:\n"+
+			"Buia:\n"+
+			"Inizio:atrio\n"+
+			"Vincente:biblioteca\n"+
+			"Mago: \n"+
+			"Strega: \n"+
+			"Cane:\n"+
+			"Attrezzi:lanterna 2 atrio\n"+
+			"Uscite:\n";
+	
+	private CaricatoreLabirinto caricatore;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void testBilocaleAttrezzi() throws FormatoFileNonValidoException, FileNotFoundException {
+		caricatore = new CaricatoreLabirinto(new StringReader(bilocale));
+		caricatore.carica();
+		Attrezzo attrezzo = new Attrezzo("lanterna", 3);
+		assertEquals(attrezzo, this.caricatore.getStanzaIniziale().getAttrezzo("lanterna"));
+		}
+
+	@Test
+	public void testBilocale() throws FormatoFileNonValidoException, FileNotFoundException {
+		caricatore = new CaricatoreLabirinto(new StringReader(bilocale));
+		caricatore.carica();
+		assertEquals("atrio", this.caricatore.getStanzaIniziale().getNome());
+		}
+}
---
+++ test/it/uniroma3/diadia/comandi/ComandoPosaTest.java
@@ -0,0 +1,40 @@
+package it.uniroma3.diadia.comandi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+
+public class ComandoPosaTest {
+
+	private Partita p;
+	private Attrezzo attrezzo;
+	private Comando comando;
+	private Labirinto labirinto;
+	@BeforeEach
+	public void setUp(){
+		p = new Partita(labirinto);
+		attrezzo = new Attrezzo("martello", 2);
+		comando = new ComandoPosa();
+	}
+
+	@Test
+	public void testAttrezzoPosato() {
+		p.getGiocatore().getBorsa().addAttrezzo(attrezzo);
+		comando.setParametro("martello");
+		comando.esegui(p);
+		assertTrue(p.getStanzaCorrente().hasAttrezzo("martello"));
+	}
+
+	@Test
+	public void testAttrezzoPosatoNull() {
+		comando.setParametro("lanterna");
+		comando.esegui(p);
+		assertFalse(p.getStanzaCorrente().hasAttrezzo("lanterna"));
+	}
+}
